b208c221322bc0cf64be2555a0e7fbb587db1fff
Karl Swedberg <kswedberg@gmail.com>
2012-01-27 18:47:46 -0500

    replaced plugins link in header with jQuery Mobile link

--- htdocs/wp-content/themes/jquery.0002/header.php
+++ htdocs/wp-content/themes/jquery.0002/header.php
@@ -26,8 +26,8 @@
         <div id="jq-primaryNavigation">
           <ul>
 <li class="jq-jquery jq-current"><a href="http://jquery.com/" title="jQuery Home">jQuery</a></li>
-<li class="jq-plugins"><a href="http://plugins.jquery.com/" title="jQuery Plugins">Plugins</a></li>
 <li class="jq-ui"><a href="http://jqueryui.com/" title="jQuery UI">UI</a></li>
+<li class="jq-mobile"><a href="http://jquerymobile.com/" title="jQuery Mobile">Mobile</a></li>
 <li class="jq-meetup"><a href="http://meetups.jquery.com/" title="jQuery Meetups">Meetups</a></li>
 <li class="jq-forum"><a href="http://forum.jquery.com/" title="jQuery Forum">Forum</a></li>
 <li class="jq-blog"><a href="http://blog.jquery.com/" title="jQuery Blog">Blog</a></li>
